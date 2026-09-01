2759f581bc91b57c393e444e67a9b752be86f372
saes2020-bit <saes2020@gmail.com>
2026-08-31 21:09:33 -0600

    syntax fix 2

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 name = input("what is your name ")
 print("Hello fucking, ", name)
 age = int(input("how old are you? "))
-if age >= 18 
-print("you are closer to death than you think ")
+if age >= 18:
+    print("you are closer to death than you think ")
 else: 
-print("you have no idea what's coming ")
+    print("you have no idea what's coming ")
