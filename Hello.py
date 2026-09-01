ed9664ca22eae68bd394d60c1752ffc2405b7b28
saes2020-bit <saes2020@gmail.com>
2026-08-31 21:06:06 -0600

    Fix conditional syntax

--- Hello.py
+++ Hello.py
@@ -1,5 +1,7 @@
 name = input("what is your name ")
 print("Hello fucking, ", name)
 age = int(input("how old are you? "))
-if age >= 18 print("you are closer to death than you think ")
-else print("you have no idea what's coming ")
+if age >= 18 
+print("you are closer to death than you think ")
+else: 
+print("you have no idea what's coming ")
